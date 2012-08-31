2d462ad5ea3e7db378046c73849e6d804f53eb37
Andreas Joseph Krogh <andreak@officenet.no>
2012-08-31 09:52:45 +0200

    - Added sonatype_snapshots repo. - Added a link which also updates the DOM in comet-response to this also triggers the AJAX-404-error

--- src/main/scala/no/officenet/example/lift/comet/CometTest.scala
+++ src/main/scala/no/officenet/example/lift/comet/CometTest.scala
@@ -4,12 +4,20 @@ import net.liftweb.http.{SHtml, CometActor}
 import net.liftweb.http.js.JE.{Str, Call}
 import java.util.Date
 import xml.Text
+import net.liftweb.http.js.JsCmds.SetHtml
 
 class CometTest extends CometActor {
 
+	case class Msg(msg: String)
+
+	override def lowPriority = {
+		case Msg(s) => partialUpdate(SetHtml("cometResponse", Text(s)))
+	}
+
 	def render = {
 		".cometContent" #> (
-			"div" #> SHtml.a(() => {Call("console.log", Str("Called at " + new Date())).cmd}, Text("Click me"))
+			"@a" #> SHtml.a(() => {Call("console.log", Str("Called at " + new Date())).cmd}, Text("Click me")) &
+			"@b" #> SHtml.a(() => {this ! Msg("Updated at " + new Date())}, Text("Click me for updating time"))
 			)
 	}
 }
